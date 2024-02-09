ed9ee87d81938d63c0bbb50bdd89f62f27ed5471
Raqiya Al Moqbali <154899570+Raqiya-AlMoqbali@users.noreply.github.com>
2024-02-09 20:32:11 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Evaluate News NLP
+
+## Overview: 
+Evaluate News NLP is a web application that analyzes the sentiment of articles using Natural Language Processing (NLP) techniques. It allows users to submit article URLs, which are then analyzed to provide insights such as sentiment, confidence, agreement, and irony.
+
+## Features 
+- Setting up Webpack
+- Sass styles
+- Webpack Loaders and Plugins
+- Creating layouts and page design
+- Service workers
+- Using APIs and creating requests to external urls
+
+## Running The Project
+### Install dependencies:
+cd evaluate-news-nlp
+npm install
+
+### Start the server:
+npm start
+
+### Build project:
+npm run build-prod
+
+## Usage
+- Enter the URL of the article you want to analyze in the provided input field.
+- Click the "Submit" button to trigger the sentiment analysis.
+- View the sentiment analysis results displayed on the page.
+
+## Contributing
+Contributions are welcome! Feel free to open issues or submit pull requests to contribute to this project.
---
+++ webpack.dev.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+            inject: 'body'
+        }),
+        new CleanWebpackPlugin({
+          dry: true,
+          verbose:true,
+          cleanStaleWebpackAssets:true,
+          protectWebpackAssets: false
+  
+        }),
+      
+       new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        })
+    ]
+}
+    /*
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
+*/
